3609c30008dafd6ad08b3cfa4454aa0dd0e9e0bd
Nola Dodd <noladodd.code@gmail.com>
2024-07-17 12:05:54 -0700

    mobile nav + xs size

--- index.js
+++ index.js
@@ -11,84 +11,82 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden')
 hiddenElements.forEach((el) => observer.observe(el))
   
+//Light and Dark Mode
 document.addEventListener("DOMContentLoaded", () => {
-  const sunButton = document.querySelector(".sun");
-  const moonButton = document.querySelector(".moon");
+  const sunButton = document.querySelectorAll(".sun");
+  const moonButton = document.querySelectorAll(".moon");
   const textAnimationSpan = document.querySelector(".text-animation span");
-
-
-  sunButton.addEventListener("click", () => {
-    // Change body background
-    const body = document.querySelector("body");
-    body.classList.add("bodylight")
-    body.classList.remove("bodydark")
-    
-    // Change module background to light mode
-    const moduleWraps = document.querySelectorAll(".module");
-    moduleWraps.forEach(module => {
-      module.classList.add("modulewrap")
-      module.classList.remove("modulewrapdark") 
-  })
   
-  const pSelect = document.querySelectorAll("p")
-  pSelect.forEach(p => {
-      p.style.color = "#040c44"
-  })
+  //to Light Mode
+  sunButton.forEach((sun) => {
+    sun.addEventListener("click", () => {
 
-  const cardSelect = document.querySelectorAll(".card")
-  cardSelect.forEach(card => {
-      card.classList.remove("bodydark")
-      card.classList.remove("border")
-      card.classList.remove("border-light")
-  })
-
-    textAnimationSpan.classList.add("light-mode");
-    textAnimationSpan.classList.remove("dark-mode");
-
-    console.log("Light Mode");
-  });
-
-  sunButton.addEventListener('click', function() {
-    sunButton.parentNode.style.display = 'none'; 
-    moonButton.parentNode.style.display = 'block'; 
-});
-
-
-  moonButton.addEventListener("click", () => {
-    // Change body background
-      const body = document.querySelector("body")
-
-      body.classList.add("bodydark")
-      body.classList.remove("bodylight")
-
-    // Change module background to dark mode
+      const body = document.querySelector("body");
+      body.classList.add("bodylight")
+      body.classList.remove("bodydark")
+      
       const moduleWraps = document.querySelectorAll(".module");
       moduleWraps.forEach(module => {
-          module.classList.add("modulewrapdark")
-          module.classList.remove("modulewrap") 
-      })
+        module.classList.add("modulewrap")
+        module.classList.remove("modulewrapdark") 
+    })
+    
+    const pSelect = document.querySelectorAll("p")
+    pSelect.forEach(p => {
+        p.style.color = "#040c44"
+    })
 
-      // Change text
-      const pSelect = document.querySelectorAll("p")
-      pSelect.forEach(p => {
-          p.style.color = "#ffff"
-      })
+    const cardSelect = document.querySelectorAll(".card")
+    cardSelect.forEach(card => {
+        card.classList.remove("border")
+        card.classList.remove("border-light")
+    })
 
-      const cardSelect = document.querySelectorAll(".card")
-      cardSelect.forEach(card => {
-          card.classList.add("bodydark")
-          card.classList.add("border")
-          card.classList.add("border-light")
-      })
+      textAnimationSpan.classList.add("light-mode");
+      textAnimationSpan.classList.remove("dark-mode");
+      console.log("Light Mode");
+    });
 
-      textAnimationSpan.classList.add("dark-mode");
-      textAnimationSpan.classList.remove("light-mode");
+})
+  
+  //to Dark Mode
+  moonButton.forEach((moon) => {
+    moon.addEventListener("click", () => {
+        const body = document.querySelector("body")
 
-    console.log("Dark Mode");
-  });
+        body.classList.add("bodydark")
+        body.classList.remove("bodylight")
+
+        const moduleWraps = document.querySelectorAll(".module");
+        moduleWraps.forEach(module => {
+            module.classList.add("modulewrapdark")
+            module.classList.remove("modulewrap") 
+        })
+
+        const pSelect = document.querySelectorAll("p")
+        pSelect.forEach(p => {
+            p.style.color = "#ffff"
+        })
+
+        const cardSelect = document.querySelectorAll(".card")
+        cardSelect.forEach(card => {
+            card.classList.add("border")
+            card.classList.add("border-light")
+        })
+
+        textAnimationSpan.classList.add("dark-mode");
+        textAnimationSpan.classList.remove("light-mode");
+
+      console.log("Dark Mode");
+    });
+
+})
+
+
 });
 
-// Swap Sun and Moon on Click
+
+//Swap Desktop Nav Sun and Moon on click 
 document.addEventListener('DOMContentLoaded', function() {
   const sunButton = document.querySelector('.sun');
   const moonButton = document.querySelector('.moon');
@@ -106,6 +104,48 @@ document.addEventListener('DOMContentLoaded', function() {
   moonButton.parentNode.style.display = 'none'; 
 });
 
+
+// Swap X and Menu icon on Click, Display mobilenav bar on click
+document.addEventListener('DOMContentLoaded', function() {
+  const menuButton = document.getElementById('menuicon');
+  const xButton = document.getElementById('xicon');
+  const navbar = document.querySelector('.mobilenav');
+
+ 
+  menuButton.addEventListener('click', function() {
+    menuButton.parentNode.style.display = 'none'; 
+    xButton.parentNode.style.display = 'block'; 
+  });
+
+
+  xButton.addEventListener('click', function() {
+    xButton.parentNode.style.display = 'none'; 
+    menuButton.parentNode.style.display = 'block'; 
+  });
+
+  menuButton.addEventListener('click', function() {
+      navbar.classList.add('active');
+  });
+
+  xButton.addEventListener('click', function() {
+      navbar.classList.remove('active')
+  })  
+
+  xButton.parentNode.style.display = 'none'; 
+
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const logo = document.getElementById('logo');
+
+  logo.addEventListener('click', function(event) {
+    event.preventDefault(); 
+
+    window.location.href = '#';
+  });
+
+});
 
 document.addEventListener('DOMContentLoaded', function() {
   const stableText = document.querySelector('.text-stable');
@@ -128,25 +168,5 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   pauseButton.parentNode.style.display = 'none'
-
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  const menuIcon = document.getElementById('menuicon');
-  const navbar = document.querySelector('.navbar');
-
-  menuIcon.addEventListener('click', function() {
-      navbar.classList.toggle('active');
-  });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  const logo = document.getElementById('logo');
-
-  logo.addEventListener('click', function(event) {
-    event.preventDefault(); 
-
-    window.location.href = '#';
-  });
 
 });
